c18ee19bb1ce9827a181ba8ddc3a4c511a58c294
kruczjak <kruczjak@gmail.com>
2017-05-21 10:30:47 +0200

    Add streaming

--- src/main/scala/ReadBookActor.scala
+++ src/main/scala/ReadBookActor.scala
@@ -1,12 +1,23 @@
+import akka.NotUsed
 import akka.actor.Actor
+import akka.stream.{ActorMaterializer, OverflowStrategy, ThrottleMode}
+import akka.stream.scaladsl.{Sink, Source}
 
-case class ReadBook(title: String)
+import scala.concurrent.duration._
+import scala.io.Source.fromFile
+
+case class ReadBook(filename: String)
 case class BookLine(line: String)
 
 class ReadBookActor extends Actor {
   override def receive: Receive = {
-    case ReadBook => {
-
-    }
+    case ReadBook(filename) =>
+      val materializer = ActorMaterializer.create(context)
+      val sink = Source.actorRef(1000, OverflowStrategy.dropNew)
+        .throttle(1, 1 second, 1, ThrottleMode.shaping)
+        .to(Sink.actorRef(sender, NotUsed))
+        .run()(materializer)
+      val lines = fromFile(filename).getLines
+      lines.foreach((line: String) => sink ! BookLine(line))
   }
 }
